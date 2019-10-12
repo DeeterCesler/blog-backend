6d3b538cf10042927b0db91b869d14ba7f1e4bb9
Deeter <deeter.cesler@gmail.com>
2019-10-12 16:04:58 -0600

    updated edit URL

--- email-templates/templateZero.js
+++ email-templates/templateZero.js
@@ -3,7 +3,7 @@ const templateZero = (userEmail, contactEmail, contactName, contactSummary) => {
     `<b>Hi ${userEmail},</b>
     <br/>
     <p>According to the reminder you sent, it's time to reach out to <strong>${contactName}</strong>. Their email is <a href="mailto:${contactEmail}">${contactEmail}</a>.</p>
-    <p>Here's what you said it's important to know about them, or what you discussed recently: <br/> ${contactSummary}. <br/> <br/> If you want to update what you talked about, you can <a href="#">do that here</a>.</p>`;
+    <p>Here's what you said it's important to know about them, or what you discussed recently: <br/> ${contactSummary}. <br/> <br/> If you want to update what you talked about, you can <a href="${process.env.REACT_APP_ADDRESS + "/contacts/all"}">do that here</a>.</p>`;
     return text;
 }
 
