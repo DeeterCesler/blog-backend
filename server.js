326dcdfb8f549e2bcc333927f38ff6b83375007c
Deeter <deeter.cesler@gmail.com>
2019-11-02 11:58:24 -0600

    cors fix inshallah

--- server.js
+++ server.js
@@ -37,7 +37,12 @@ const corsOptions = {
   }
   
 // Middleware
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    next();
+  });
 app.use(methodOverride("_method"));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
